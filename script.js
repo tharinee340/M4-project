49b30c053ce0e1f509f392a4553be8b020b74a70
Tharinee Thuengnok <tharinee_t@cmu.ac.th>
2021-06-24 14:24:36 +0700

    02. add fav anime
    double click and add anime to backend

--- script.js
+++ script.js
@@ -11,63 +11,83 @@ function searchAnime(event) {
             console.log(element)
             addAnimeToCard(element)
         })
-        //addAnimeToCard
-        //console.log(data.results)
     }).catch((err) => {
         console.warn(err.message)
     })
 }
-/*
-function updateDom(data){
 
-    const searchResults = document.getElementById('output')
-    const animeCategories = data.results
-        .reduce((acc,anime) => {
-            const {type} = anime
-            if(acc[type] == undefined) acc[type] = []
-            acc[type].push(anime)
-            return acc
-        }, {})
-    
-    searchResults.innerHTML = Object.keys(animeCategories).map(key => {
-        const animesHTML = animeCategories[key]
-        .sort((a,b) => a.episodes-b.episodes)
-        .map(anime => console.log(anime))
-    })
-        
-    //showSearchMovie(anime)
-}
-
-
-*/
 function onLoad(){
     document.getElementById('searchButton')
     .addEventListener('click', searchAnime)
+
 }
 
-function addAnimeToData(anime) {
+function addAnimeToCard(anime) {
+    
+    const searchList = document.getElementById('searchList')
+    
+    let row = document.createElement('row')
+    let card = document.createElement('div')
+    
+
+    let img = document.createElement('img')
+    img.setAttribute('src',anime.image_url)
+    img.addEventListener('dblclick',(event) => {
+        let confirmMsg = confirm(`ท่านต้องการเพิ่มอนิเมะเรื่อง ${anime.title} เป็นรายการโปรดหรือไม่`)
+        if (confirmMsg){
+            addAnimeToDB(anime) 
+        }
+
+    })
+
+    card.appendChild(img) 
+    let titleAnime = document.createElement('h5')
+    titleAnime.innerHTML = `${anime.title}`
+    
+    card.appendChild(titleAnime)
+    searchList.appendChild(card)
+    
+
+}
+
+function addFavAnimeToDB() {
     let imageElem = document.getElementById('image')
     imageElem.setAttribute('src',anime.image_url)
     let titleElem = document.getElementById('title')
     titleElem.innerHTML = anime.title
 }
 
-function addAnimeToCard(anime) {
-    const searchList = document.getElementById('searchList')
-    
-    let row = document.createElement('row')
-    let card = document.createElement('div')
+function addAnimeToDB(anime) {
 
-    let img = document.createElement('img')
-    img.setAttribute('src',anime.image_url)
-    card.appendChild(img) 
-    let titleAnime = document.createElement('h5')
-    titleAnime.innerHTML = `${anime.title}`
-    
-    
-    card.appendChild(titleAnime)
-    searchList.appendChild(card)
-
+    var id = 1
+    let fav = `{ 
+        "url" : "${anime.url}",
+        "image_url" : "${anime.image_url}",
+        "title" : "${anime.title}",
+        "synopsis" : "${anime.synopsis}",
+        "type" : "${anime.type}",
+        "episodes" : "${anime.episodes}",
+        "score" : "${anime.score}",
+        "rated" : "${anime.rated}",
+        "id" : "${id}"
+    }`
+    fetch('https://se104-project-backend.du.r.appspot.com/movies', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: `{"id":"632110340","movie":${fav}}`
+    }).then(response => {
+        if (response.status === 200) {
+            return response.json()
+        } else {
+            throw Error(response.statusText)
+        }
+    }).then(data => {
+        alert('Add favorite success')
+        id++
+    }).catch(error => {
+        alert('Error cannot at favorite')
+    })
 }
-
 
